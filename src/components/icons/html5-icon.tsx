f4e531b60c1c4055581fba246276870f64f2be03
Ricardo Quadros <ricardaoquadros@gmail.com>
2025-09-29 16:08:49 +0000

    Agora o html e css3 estão pretas

--- src/components/icons/html5-icon.tsx
+++ src/components/icons/html5-icon.tsx
@@ -3,12 +3,12 @@ export function Html5Icon(props: React.SVGProps<SVGSVGElement>) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      fill="currentColor"
       {...props}
     >
-      <path d="M1.6 0h20.8l-1.9 21.6-8.5 2.4-8.5-2.4L1.6 0z" />
-      <path d="M12 2.15l7.6 2.1-1.7 19.2-5.9 1.65v-21z" />
-      <path d="M12 4.35v15.4l5.1-.05.15-1.7h-3.3l.2-2.15h3.1l.3-3.4h-6.2l.2-2.15h6l.3-3.6H12z" />
+      <path fill="#E34F26" d="M1.6 0h20.8l-1.9 21.6-8.5 2.4-8.5-2.4L1.6 0z" />
+      <path fill="#F16529" d="M12 2.15l7.6 2.1-1.7 19.2-5.9 1.65v-21z" />
+      <path fill="#EBEBEB" d="M12 4.35v15.4l5.1-.05.15-1.7h-3.3l.2-2.15h3.1l.3-3.4h-6.2l.2-2.15h6l.3-3.6H12z" />
+      <path fill="#FFFFFF" d="M12 4.35v15.4l-5.1-.05L6.75 18H10l-.2 2.15-3.1.05-.3 3.4h6.2l-.2-2.15H6l-.3-3.6h6.2V4.35H12z" />
     </svg>
   );
 }
