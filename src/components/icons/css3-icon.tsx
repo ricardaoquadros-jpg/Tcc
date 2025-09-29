000332fc0fa8583d9972b9e6575b67ec1ad769ed
Ricardo Quadros <ricardaoquadros@gmail.com>
2025-09-29 16:07:46 +0000

    and I also suspect there's an issue with how they are being rendered., e

--- src/components/icons/css3-icon.tsx
+++ src/components/icons/css3-icon.tsx
@@ -1,12 +1,14 @@
 export function Css3Icon(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
-      <path fill="#1572B6" d="M1.6 0h20.8l-1.9 21.6-8.5 2.4-8.5-2.4L1.6 0z" />
-      <path fill="#33A9DC" d="M12 2.15l7.6 2.1-1.7 19.2-5.9 1.65v-21z" />
-      <path
-        fill="#FFFFFF"
-        d="M12 4.35v15.4l5.1-.05.15-1.7h-3.3l.2-2.15h3.1l.3-3.4h-6.2l.2-2.15h6l.3-3.6H12z"
-      />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      {...props}
+    >
+      <path d="M1.6 0h20.8l-1.9 21.6-8.5 2.4-8.5-2.4L1.6 0z" />
+      <path d="M12 2.15l7.6 2.1-1.7 19.2-5.9 1.65v-21z" />
+      <path d="M12 4.35v15.4l5.1-.05.15-1.7h-3.3l.2-2.15h3.1l.3-3.4h-6.2l.2-2.15h6l.3-3.6H12z" />
     </svg>
   );
 }
