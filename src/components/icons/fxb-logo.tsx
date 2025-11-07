98b80a4c54b4c328f9e8d956003fdb919ead8873
Ricardo Quadros <ricardaoquadros@gmail.com>
2025-11-07 01:46:30 +0000

    Make the following changes:
    Find this element:
    
      <text x="50%" y="50%"

--- src/components/icons/fxb-logo.tsx
+++ src/components/icons/fxb-logo.tsx
@@ -1,23 +1,17 @@
 export function FXBLogo(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
-      viewBox="0 0 100 100"
+      viewBox="0 0 24 24"
       fill="none"
+      stroke="currentColor"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <text
-        x="50%"
-        y="50%"
-        dominantBaseline="middle"
-        textAnchor="middle"
-        fontSize="60"
-        fontWeight="bold"
-        fill="currentColor"
-        className="font-headline"
-      >
-        FXB
-      </text>
+      <polyline points="16 18 22 12 16 6" />
+      <polyline points="8 6 2 12 8 18" />
     </svg>
   );
 }
